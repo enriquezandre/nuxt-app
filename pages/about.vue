dbde822d17ffd211008b9c3231f045174251a776
Andre Enriquez <andre.enriquez@cit.edu>
2023-07-03 09:34:57 +0800

    First commit

---
+++ pages/about.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>About</h2>
+    <p>
+      Loremipsum dolor, sit amet consectetur adipisicing elit. Dolorum ipsa sunt
+      veritatis quod, quo ullam molestias enim officia illo minus!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum ipsa
+      sunt veritatis quod, quo ullam molestias enim officia illo minus!
+    </p>
+    <div>{{ data }}</div>
+  </div>
+</template>
+
+<script setup>
+const { data } = await useFetch("/api/currency/GBP");
+// ?name=mario", {
+//   method: "post",
+//   body: { age: 30 },
+// });
+</script>
+
+<style scoped>
+h2 {
+  margin-bottom: 20px;
+  font-size: 36px;
+}
+p {
+  margin: 20px 0;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum ipsa
+      sunt veritatis quod, quo ullam molestias enim officia illo minus!
+    </p>
+    <p>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorum ipsa
+      sunt veritatis quod, quo ullam molestias enim officia illo minus!
+    </p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
